14f3d3ac954e3f72e776cda9b4a58509abf8f492
YooYeonJun <72008448+YooYeonJun@users.noreply.github.com>
2024-10-25 23:11:17 +0900

    Update GTBlueprintFunctionLibrary.cpp

--- Source/GASToy/GTBlueprintFunctionLibrary.cpp
+++ Source/GASToy/GTBlueprintFunctionLibrary.cpp
@@ -33,19 +33,19 @@ EGTDirection UGTBlueprintFunctionLibrary::CalculateDirectionType(const float Deg
 
 FVector UGTBlueprintFunctionLibrary::CalculateDirectionVector(const EGTDirection Direction)
 {
-	const UEnum* DirectionEnum = FindObjectChecked<UEnum>(nullptr, TEXT("/Script/GASToy.EGTDirection"), true);
-	
-	static TMap<EGTDirection, FVector> DirectionToVectorMap;
-	DirectionToVectorMap.Reserve(DirectionEnum->NumEnums());
-	DirectionToVectorMap.Add(EGTDirection::None, FVector::ZeroVector);
-	DirectionToVectorMap.Add(EGTDirection::Forward, FVector::ForwardVector);
-	DirectionToVectorMap.Add(EGTDirection::ForwardLeft, (FVector::ForwardVector + FVector::LeftVector).GetUnsafeNormal());
-	DirectionToVectorMap.Add(EGTDirection::ForwardRight, (FVector::ForwardVector + FVector::RightVector).GetUnsafeNormal());
-	DirectionToVectorMap.Add(EGTDirection::Backward, FVector::BackwardVector);
-	DirectionToVectorMap.Add(EGTDirection::BackwardLeft, (FVector::BackwardVector + FVector::LeftVector).GetUnsafeNormal());
-	DirectionToVectorMap.Add(EGTDirection::BackwardRight, (FVector::BackwardVector + FVector::RightVector).GetUnsafeNormal());
-	DirectionToVectorMap.Add(EGTDirection::Left, FVector::LeftVector);
-	DirectionToVectorMap.Add(EGTDirection::Right, FVector::RightVector);
+	static TMap<EGTDirection, FVector> DirectionToVectorMap =
+		{
+			{EGTDirection::None, FVector::ZeroVector},
+			{EGTDirection::Forward, FVector::ForwardVector},
+			{EGTDirection::ForwardLeft, (FVector::ForwardVector + FVector::LeftVector).GetUnsafeNormal()},
+			{EGTDirection::ForwardRight, (FVector::ForwardVector + FVector::RightVector).GetUnsafeNormal()},
+			{EGTDirection::Backward, FVector::BackwardVector},
+			{EGTDirection::BackwardLeft, (FVector::BackwardVector + FVector::LeftVector).GetUnsafeNormal()},
+			{EGTDirection::BackwardRight, (FVector::BackwardVector + FVector::RightVector).GetUnsafeNormal()},
+			{EGTDirection::Left, FVector::LeftVector},
+			{EGTDirection::Right, FVector::RightVector}
+		};
 
 	return DirectionToVectorMap[Direction];
 }
+
